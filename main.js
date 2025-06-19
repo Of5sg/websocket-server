14cf53061478f85c029b5ac044fb22658a577167
Of5sg <aksel.kla@gmail.com>
2025-06-19 07:15:48 +0000

    correct mistake in name

---
+++ main.js
@@ -0,0 +1,43 @@
+import net from "net";
+import { Buffer } from "buffer";
+
+/**      0                   1                   2                   3
+      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     +-+-+-+-+-------+-+-------------+-------------------------------+
+     |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+     |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+     |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+     | |1|2|3|       |K|             |                               |
+     +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+     |     Extended payload length continued, if payload len == 127  |
+     + - - - - - - - - - - - - - - - +-------------------------------+
+     |                               |Masking-key, if MASK set to 1  |
+     +-------------------------------+-------------------------------+
+     | Masking-key (continued)       |          Payload Data         |
+     +-------------------------------- - - - - - - - - - - - - - - - +
+     :                     Payload Data continued ...                :
+     + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+     |                     Payload Data continued ...                |
+     +---------------------------------------------------------------+ */
+
+     //https://datatracker.ietf.org/doc/html/rfc6455#section-5.2
+
+const server = net.createServer((socket) => {
+
+
+    socket.on("data", (data) => {
+
+        data.readUintBE(9, 7);
+
+        console.log(data);
+    });
+
+    socket.on("end", () => {
+        console.log("client disconnected");
+    })
+
+});
+
+server.listen(8000, () => {
+    console.log("server started on port 8000");
+});
