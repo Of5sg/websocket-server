071becb267a7aa2dc772fb063b88aecc6330a250
Of5sg <aksel.kla@gmail.com>
2025-07-01 07:54:30 +0000

    utvikling

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
 import net from "net";
 import { Buffer } from "buffer";
 import crypto from "crypto";
+import { isReadable } from "stream";
 
-
-/*
-for testing
-
-nc localhost 8000
-
-GET / HTTP/1.1\r\n
-Host: localhost:8000\r\n
-Upgrade: websocket\r\n
-Connection: upgrade\r\n
-Sec-WebSocket-Key: dGhlIHNhbXBsZSB\r\n
-Sec-WebSocket-Version: 13\r\n\r\n
-*/
-
-
- // https://nodejs.org/api/cluster.html
- // bruke workere, og cluster????
 
 /**   0                   1                   2                   3
       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -42,116 +26,131 @@ Sec-WebSocket-Version: 13\r\n\r\n
 
 //https://datatracker.ietf.org/doc/html/rfc6455#section-5.2
 
-const server = net.createServer((socket) => {
+async function getChunk(socket) {
 
-    socket.setEncoding("utf8");
+    let fullBuffer = Buffer.alloc(0);
 
-    // socket.once("data", (data) => {
+    const endSignal = Buffer.from([13, 10, 13, 10]);
 
-    //     console.log(data);
+    for await (const chunk of socket) {
 
+        fullBuffer = Buffer.concat([fullBuffer, chunk]);
 
+        if(Buffer.compare(Buffer.copyBytesFrom(fullBuffer, fullBuffer.byteLength-4, 4), endSignal) === 0){
 
-    //     // handshake
-    //     // https://datatracker.ietf.org/doc/html/rfc6455#section-4.2.1
-        
-    //     /**4.2.1.  Reading the Client's Opening Handshake
+            return fullBuffer;
 
-    //         When a client starts a WebSocket connection, it sends its part of the
-    //         opening handshake.  The server must parse at least part of this
-    //         handshake in order to obtain the necessary information to generate
-    //         the server part of the handshake.
+        };
+    };
 
-    //         The client's opening handshake consists of the following parts.  If
-    //         the server, while reading the handshake, finds that the client did
-    //         not send a handshake that matches the description below (note that as
-    //         per [RFC2616], the order of the header fields is not important),
-    //         including but not limited to any violations of the ABNF grammar
-    //         specified for the components of the handshake, the server MUST stop
-    //         processing the client's handshake and return an HTTP response with an
-    //         appropriate error code (such as 400 Bad Request).
+};
 
-    //         1.   An HTTP/1.1 or higher GET request, including a "Request-URI"
-    //                 [RFC2616] that should be interpreted as a /resource name/
-    //                 defined in Section 3 (or an absolute HTTP/HTTPS URI containing
-    //                 the /resource name/).
+function splitLines(incommingBuff){
 
-    //         2.   A |Host| header field containing the server's authority.
+    // convert to strings
 
-    //         3.   An |Upgrade| header field containing the value "websocket",
-    //                 treated as an ASCII case-insensitive value.
+    const stringified = incommingBuff.toString();
+    const split = stringified.split("\r\n");
 
-    //         4.   A |Connection| header field that includes the token "Upgrade",
-    //                 treated as an ASCII case-insensitive value.
+    const requestObj = {};
 
-    //         5.   A |Sec-WebSocket-Key| header field with a base64-encoded (see
-    //                 Section 4 of [RFC4648]) value that, when decoded, is 16 bytes in
-    //                 length.
+    for (const parameter of split){
 
-    //         6.   A |Sec-WebSocket-Version| header field, with a value of 13.
+        // split http handshake request to key/value
+        if(parameter !== "GET / HTTP/1.1" && parameter !== ""){
 
-    //         7.   Optionally, an |Origin| header field.  This header field is sent
-    //                 by all browser clients.  A connection attempt lacking this
-    //                 header field SHOULD NOT be interpreted as coming from a browser
-    //                 client.
+            const temp = parameter.split(": ");
 
-    //         8.   Optionally, a |Sec-WebSocket-Protocol| header field, with a list
-    //                 of values indicating which protocols the client would like to
-    //                 speak, ordered by preference.
+            // replace all - with _ , so i don't have to have strings as keys.
+            requestObj[temp[0].replaceAll("-", "_")] = temp[1];
 
-    //         9.   Optionally, a |Sec-WebSocket-Extensions| header field, with a
-    //                 list of values indicating which extensions the client would like
-    //                 to speak.  The interpretation of this header field is discussed
-    //                 in Section 9.1.
+        }else if(parameter !== ""){
 
-    //         10.  Optionally, other header fields, such as those used to send
-    //                 cookies or request authentication to a server.  Unknown header
-    //                 fields are ignored, as per [RFC2616]. 
-                    
-    //     */
+            const temp = parameter.split(" ");
+            requestObj["method"] = temp[0];
+            requestObj["path"] = temp[1];
+            requestObj["protocol"] = temp[2];
+
+        };
+
+    };
+
+    return requestObj;    
+}
+
+const server = net.createServer(async(socket) => {
 
 
-    // });
 
-    socket.on("data", (data) => {
+    console.log("is readable? :", isReadable(socket), "\n")
 
+    // https://nodejs.org/api/stream.html#readableforeachfn-options
+    // https://nodejs.org/api/stream.html#readable-streams
 
-        // https://datatracker.ietf.org/doc/html/rfc6455#section-5.2
+    if(isReadable(socket)){
 
-        /*   0                   1                   2                   3
-             0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-            +-+-+-+-+-------+-+-------------+-------------------------------+
-            |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
-            |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
-            |N|V|V|V|       |S|             |   (if payload len==126/127)   |
-            | |1|2|3|       |K|             |                               |
-            +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-            |     Extended payload length continued, if payload len == 127  |
-            + - - - - - - - - - - - - - - - +-------------------------------+
-            |                               |Masking-key, if MASK set to 1  |
-            +-------------------------------+-------------------------------+
-            | Masking-key (continued)       |          Payload Data         |
-            +-------------------------------- - - - - - - - - - - - - - - - +
-            :                     Payload Data continued ...                :
-            + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-            |                     Payload Data continued ...                |
-            +---------------------------------------------------------------+ 
-        */
+        // buffer er i hex
+        const incommingBuff = await getChunk(socket);
 
-        // data.readUintBE(9, 7);
+        const requestObj = splitLines(incommingBuff);
 
-        // socket.write()
+        console.log("Request:");
+        console.log(requestObj);
 
-        console.log("remote address \t", socket.remoteAddress, "\n", data);
-        
+        // create response-key
+        const acceptKey = crypto
+            .createHash("sha1")
+            .update(requestObj.sec_websocket_key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+            .digest("base64");
+
+        // https://datatracker.ietf.org/doc/html/rfc6455#section-1.3
+
+        // create response
+        const response = [
+            "HTTP/1.1 101 Switching Protocols",
+            // `origin: ${requestObj.origin}`,
+            `upgrade: websocket`,
+            `connection: upgrade`,
+            `sec-websocket-accept: ${acceptKey}`,
+            `sec-websocket-version: ${requestObj.sec_websocket_version}`,
+            `sec-websocket-extensions: ${requestObj.sec_websocket_extensions.split("; ")[0]}`,
+            // `subprotocol: null`,
+            // `extensions: []`,
+             "\r\n"
+            ].join("\r\n");
+
+        console.log(`\nResponse:\n${response}`);
+
+        socket.write(Buffer.from(response));
+
+    }else{
+
+        throw new Error("Socket not readable.");
+
+    };
+
+    socket.once("end", (closingHandshake) => {
+
+        console.log(`-----\n\nrecieved closing handshake from:\n\n\tremoteAddress\t${socket.remoteAddress}\n\non:\n\n\tlocalPort\t${socket.localPort}\n\tlocalAddress\t${socket.localAddress}\n\n-----`);
+
     });
 
-    socket.on("end", () => {
-        console.log("client disconnected");
+    socket.on("timeout", () => {
+        console.log("Connection timed out");
+        socket.end();
+    });
+
+    socket.on("close", () => {
+
+        console.log("Connection closed.\n");
+
     });
 
 });
 
 server.listen(8000, () => {
-    console.log("server started on port 8000");
+
+    console.log("server started on port 8000\n");
+
 });
+
