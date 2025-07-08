5162f5e704873215972e7c270982f10f53e4cf9b
Of5sg <aksel.kla@gmail.com>
2025-07-08 09:34:09 +0000

    some small changes

--- main.js
+++ main.js
@@ -73,11 +73,9 @@ function bits(num){
 
 const server = net.createServer(async(socket) => {
 
-    console.log("is readable? :", isReadable(socket), "\n");
-
     let websock = false;
 
-        // when the websocket-connection is established
+    // when the websocket-connection is established
     /**  0                   1                   2                   3
          0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
         +-+-+-+-+-------+-+-------------+-------------------------------+
